4f02bb19eec797ca2d26944ed152a6c7da89b6fb
axeljemt <axel.jemt@gmail.com>
2023-11-21 22:34:24 +0100

    freak2345

--- lala.py
+++ lala.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import sqlalchemy
+import psycopg2
+
 import logging
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 logging.debug('This message should appear on the console')
@@ -9,3 +14,14 @@ st.title('Uber pickups in NYC')
 
 
 
+
+# Initialize connection.
+conn = st.connection("postgresql", type="sql")
+
+# Perform query.
+df = conn.query('SELECT * FROM table1 limit 10;', ttl="10m")
+df = conn.query('SELECT * FROM table1 limit 1;', ttl="10m")
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.colument1} has a :{row.column2}:")
