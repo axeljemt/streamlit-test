3183d73104dd395e9da4f8b634bb3e4c9388cc53
axeljemt <axel.jemt@gmail.com>
2023-11-21 22:00:48 +0100

    freak2345

--- lala.py
+++ lala.py
@@ -4,14 +4,20 @@ import numpy as np
 import psycopg2
 import sqlalchemy
 
+st.title('Uber pickups in NYC')
 
 # Initialize connection.
 conn = st.connection("postgresql", type="sql")
 
+st.title('Uber pickups in NYC2')
 # Perform query.
-df = conn.query('SELECT * FROM table_1 limit 1;', ttl="10m")
+#df = conn.query('SELECT * FROM table_1 limit 1;', ttl="10m")
+
+st.title('Uber pickups in NYC3')
 
 # Print results.
-for row in df.itertuples():
-    st.write(f"{row.colument1} has a :{row.column2}:")
+#for row in df.itertuples():
+    #st.write(f"{row.colument1} has a :{row.column2}:")
+    
+st.title('Uber pickups in NYC4')
 
