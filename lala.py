ea8af2e3dff01d036bd161b883d801f0c058374d
axeljemt <axel.jemt@gmail.com>
2023-11-21 21:39:38 +0100

    freak2

--- lala.py
+++ lala.py
@@ -7,7 +7,7 @@ import numpy as np
 conn = st.connection("postgresql", type="sql")
 
 # Perform query.
-df = conn.query('SELECT * FROM table1 limit 10;', ttl="10m")
+df = conn.query('SELECT * FROM table1 limit 1;', ttl="10m")
 
 # Print results.
 for row in df.itertuples():
