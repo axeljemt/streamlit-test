b1cf0f4434d263e05c0854153755a2e52510c2a4
axeljemt <axel.jemt@gmail.com>
2023-11-21 21:37:26 +0100

    freak

--- lala.py
+++ lala.py
@@ -3,15 +3,11 @@ import pandas as pd
 import numpy as np
 
 
-# streamlit_app.py
-
-import streamlit as st
-
 # Initialize connection.
 conn = st.connection("postgresql", type="sql")
 
 # Perform query.
-df = conn.query('SELECT * FROM table1;', ttl="10m")
+df = conn.query('SELECT * FROM table1 limit 10;', ttl="10m")
 
 # Print results.
 for row in df.itertuples():
