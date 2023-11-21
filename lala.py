943ad1d92ab82ae3c4d590384bbfc526864bba96
axeljemt <axel.jemt@gmail.com>
2023-11-21 21:11:35 +0100

    first commit3

--- lala.py
+++ lala.py
@@ -2,5 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+[connections.postgresql]
+dialect = "postgresql"
+host = "localhost"
+port = "5432"
+database = "database-1"
+username = "postgres"
+password = "Bajsbajs1"
+
 st.title('Uber pickups in NYC')
 
