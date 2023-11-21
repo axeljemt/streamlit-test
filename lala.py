6b61c12809f0a8170ea4beff6b22643629308021
axeljemt <axel.jemt@gmail.com>
2023-11-21 22:11:52 +0100

    freak2345

--- lala.py
+++ lala.py
@@ -1,18 +1,7 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-#import psycopg2
-#import sqlalchemy
+
 
 st.title('Uber pickups in NYC')
 
 
-st.title('Uber pickups in NYC2')
-
-
-st.title('Uber pickups in NYC3')
-
-
-    
-st.title('Uber pickups in NYC4')
 
