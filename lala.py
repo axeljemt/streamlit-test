8897e6f7002e4ef445522e58711bcdd824a60479
axeljemt <axel.jemt@gmail.com>
2023-11-21 22:10:05 +0100

    freak2345

--- lala.py
+++ lala.py
@@ -6,18 +6,13 @@ import sqlalchemy
 
 st.title('Uber pickups in NYC')
 
-# Initialize connection.
-#conn = st.connection("postgresql", type="sql")
 
 st.title('Uber pickups in NYC2')
-# Perform query.
-#df = conn.query('SELECT * FROM table_1 limit 1;', ttl="10m")
+
 
 st.title('Uber pickups in NYC3')
 
-# Print results.
-#for row in df.itertuples():
-    #st.write(f"{row.colument1} has a :{row.column2}:")
+
     
 st.title('Uber pickups in NYC4')
 
