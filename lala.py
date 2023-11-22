b22f30aa75c9d89e922290b30935774453578f95
axeljemt <axel.jemt@gmail.com>
2023-11-22 19:37:39 +0100

    freak2345

--- lala.py
+++ lala.py
@@ -25,7 +25,7 @@ df = conn.query('SELECT * FROM table_1;', ttl="10m")
 #for row in df.itertuples():
 #    st.write(f"{row.colument1} has a :{row.column2}:")
 
-df = load_data()
+#df = load_data()
 edited_df = st.data_editor(df) # 👈 An editable dataframe
 
 favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
